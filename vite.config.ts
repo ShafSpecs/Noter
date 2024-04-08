fb2a0174b725ac0cf56158bf0b6991d8d524c12d
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2024-04-08 17:16:11 +0100

    added `remix-pwa`

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { remixPWA } from "@remix-pwa/dev";
+import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+installGlobals();
+
+export default defineConfig({
+  plugins: [remix(), tsconfigPaths(), remixPWA()],
+});
